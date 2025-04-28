0deb15e8b17bf9d4fc37c31de128528970bdb1b8
Nahom Haileselassie <66105821+CRXNCM@users.noreply.github.com>
2025-04-28 17:04:44 +0300

    feat(helpers): add selected_movie to session state
    Initialize `selected_movie` in `init_session_state` to track the currently selected movie in the session state

--- utils/helpers.py
+++ utils/helpers.py
@@ -68,3 +68,6 @@ def init_session_state():
     
     if "current_page" not in st.session_state:
         st.session_state.current_page = 1
+        
+    if "selected_movie" not in st.session_state:
+        st.session_state.selected_movie = None
